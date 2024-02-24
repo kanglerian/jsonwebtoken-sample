bfcb5145136e0a76d92237336877c4df19bda55e
kanglerian <kanglerian@gmail.com>
2024-02-24 18:45:09 +0700

    tambah cara 2

--- index.js
+++ index.js
@@ -3,21 +3,30 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = "lerian123";
 
 /* syncronous */
-const token = jwt.sign({ data: { student: "Lerian Febriana" }}, JWT_SECRET, { expiresIn: "1m"});
+const token = jwt.sign({ data: { student: "Lerian Febriana" }}, JWT_SECRET, { expiresIn: "20s"});
 
 /* asyncronous */
-jwt.sign({ data: { student: "Lerian Febriana" }}, JWT_SECRET, { expiresIn: "1m"}, (err, token) => {
-  if(err){
-    console.log(err.message);
-    return;
-  }
-  console.log(token);
-})
+// jwt.sign({ data: { student: "Lerian Febriana" }}, JWT_SECRET, { expiresIn: "1m"}, (err, token) => {
+//   if(err){
+//     console.log(err.message);
+//     return;
+//   }
+//   console.log(token);
+// }
 
-jwt.verify(token, JWT_SECRET, (err, decoded) => {
-  if(err){
-    console.log(err.message);
-    return;
-  }
+try {
+  const decoded = jwt.verify(token, JWT_SECRET);
   console.log(decoded);
-});
+} catch (error) {
+  console.log(error.message);
+}
+
+/* asyncronous */
+// jwt.verify(token, JWT_SECRET, (err, decoded) => {
+//   if(err){
+//     console.log(err.message);
+//     return;
+//   }
+//   console.log(decoded);
+// });
+
